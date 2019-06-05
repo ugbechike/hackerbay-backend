665a390fab39f5df61e5573a52c00401bd7f49a9
chycocee1 <engr.ugbechike@gmail.com>
2019-06-05 14:09:55 +0100

    modified generator token

--- tokenCreator/Generator.js
+++ tokenCreator/Generator.js
@@ -1,15 +1,28 @@
 var jwt = require('jsonwebtoken');
-var secretKey = process.env.secretKey
+var secretKeys = process.env.secretKey
 
 class generator{
     createToken(data){
+        console.log(data , 'data in payload');
         return new Promise((resolve, reject)=>{
-            jwt.sign(data, secretKey,{expiresIn:'24hrs'}, function(err, usertoken){
+            jwt.sign(data, secretKeys,{expiresIn: '24hrs'}, function(err, usertoken){
                if(!err){
                    resolve(usertoken);
                }else{
                    reject(err);
                }
+            })
+        })
+    }
+
+    verifyToken(data){
+        return new Promise((resolve, reject)=>{
+            jwt.verify(data.replace("bearer ", ""), secretKeys, function(err, newToken){
+                if(!err){
+                    resolve(newToken)
+                }else{
+                    reject(err);
+                }
             })
         })
     }
