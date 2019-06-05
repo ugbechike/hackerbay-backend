f56767af7e5a7cef277c5abed406316e5d799fe7
chycocee1 <engr.ugbechike@gmail.com>
2019-06-05 14:11:32 +0100

    public route modified

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var user =  require('../controller/userLogin');
+var userPatchController = require('../controller/jsonpatch');
+var routeAuths = require('../routeauthenticator/authenticator');
+var userThumbNail = require('../controller/userThumbnail');
 router.post('/login', user.userLogin);
+router.patch('/userpatch', routeAuths.routeAuth, userPatchController.createPatch);
+router.get('/thumbnail', routeAuths.routeAuth, userThumbNail.thumbnailGenerator);
 
 module.exports = router;
