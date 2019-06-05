992cb7e5fe394eea93b1cd7b8685448e3135e446
chycocee1 <engr.ugbechike@gmail.com>
2019-06-05 14:03:08 +0100

    resolved issues

--- routeauthenticator/authenticator.js
+++ routeauthenticator/authenticator.js
@@ -2,12 +2,12 @@ var tokenGenerator = require('../tokenCreator/Generator');
 
 class authenticator{
     routeAuth(req,res,next){
-        var jwt = req.header['userToken'];
+        var jwt = req.headers['usertoken'];
         if(jwt == null){
             res.status(401).send({ message: "Token not available" });
 
         }else{
-           tokenGenerator.createToken(jwt).then(token =>{
+           tokenGenerator.verifyToken(jwt).then(token =>{
                next();
            }).catch(err =>{
                next(err);
